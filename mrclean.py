d6a5bb7beb7059b568915d1410bbdfcde0ae8105
Ben <benstewartbs@gmail.com>
2018-04-28 15:10:38 -0400

    For real

--- mrclean.py
+++ mrclean.py
@@ -4,7 +4,11 @@ import csv
 words = []
 with open('urban.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
+    count=0
     for row in readCSV:
+        count+=1
+        if count % 10000 == 0:
+            print(count + "/" + len(readCSV))
         description = row[5]
         if description.find(";") != -1:
             description = description[:description.find(";")]
