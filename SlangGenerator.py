2ff2e396e33f853e5052b97a4b538339ae416810
SCrawshaw18 <scott_crawshaw18@milton.edu>
2018-04-28 17:57:33 -0400

    changes

--- SlangGenerator.py
+++ SlangGenerator.py
@@ -1,4 +1,4 @@
 import os
 word = input("Make up a word and enter it here: ")
-response = str(os.system('''th sample.lua cv/some_checkpoint.t7 -primetext "%s:" -temperature 0.5'''%(word)))
+response = str(os.system('''th sample.lua cv/lm_lstm_epoch9.31_1.3900.t7 -primetext "%s:" -temperature 0.5'''%(word)))
 print(response[:response.find("\n")])
