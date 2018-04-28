f009bca2d7770040a76d88e008910e12c0ce638f
Ben <benstewartbs@gmail.com>
2018-04-28 17:05:45 -0400

    New changes

--- mrclean.py
+++ mrclean.py
@@ -1,15 +1,23 @@
 
-import csv
+# coding: utf-8
 
+# In[11]:
+
+
+import csv
+import json
 words = []
+
+bads=open("en.txt","r")
+listbads=bads.read().split("\n")
+print(listbads)
 with open('urban.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     count=0
     for row in readCSV:
         if len(row)==6:
-            count+=1
             if count % 10000 == 0:
-                print(str(count) + "/ almost 2.6 million")
+                print(str(count))
             description = row[5]
             if description.find(";") != -1:
                 description = description[:description.find(";")]
@@ -21,10 +29,13 @@ with open('urban.csv') as csvfile:
             description=description.replace("\n","")
             
             term=row[1].lower()
-            
-            if len(description)>2 and term.find("/")==-1 and count % 25 == 0:
+            mean=False
+            for bad in listbads:
+                if bad.lower() in term.lower() or bad.lower() in description.lower():
+                    mean=True
+            if len(description)>2 and term.find("/")==-1 and count % 25 == 0 and not mean:
                 words.append("%s: %s \n"%(term,description))
-
+    print(words)
 file=open("input.txt","w")
 for word in words:
     file.write(word)
