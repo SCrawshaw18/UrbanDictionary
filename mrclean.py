479e55752723329ec16b00885cdf3f2f0a199fdd
Ben <benstewartbs@gmail.com>
2018-04-28 15:17:09 -0400

    Fix

--- mrclean.py
+++ mrclean.py
@@ -6,23 +6,24 @@ with open('urban.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     count=0
     for row in readCSV:
-        count+=1
-        if count % 10000 == 0:
-            print(count + "/" + len(readCSV))
-        description = row[5]
-        if description.find(";") != -1:
-            description = description[:description.find(";")]
-        description=description.replace("1.","")
-        description=description.replace("1 ","")
-        description=description.strip()
-        description=' '.join(description.split())
-        description=description.replace(" ,",",")
-        description=description.replace("\n","")
-        
-        term=row[1].lower()
-        
-        if len(description)>2 and term.find("/")==-1:
-            words.append("%s: %s \n"%(term,description))
+        if len(row)>5:
+            count+=1
+            if count % 10000 == 0:
+                print(count + "/" + len(readCSV))
+            description = row[5]
+            if description.find(";") != -1:
+                description = description[:description.find(";")]
+            description=description.replace("1.","")
+            description=description.replace("1 ","")
+            description=description.strip()
+            description=' '.join(description.split())
+            description=description.replace(" ,",",")
+            description=description.replace("\n","")
+            
+            term=row[1].lower()
+            
+            if len(description)>2 and term.find("/")==-1:
+                words.append("%s: %s \n"%(term,description))
 
 file=open("input.txt","w")
 for word in words:
