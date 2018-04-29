7a0aac98b3be18a3a8cba42ea59401976ebebbd8
SCrawshaw18 <scott_crawshaw18@milton.edu>
2018-04-29 17:12:39 -0400

    edits

--- SlangGenerator.py
+++ SlangGenerator.py
@@ -1,4 +1,13 @@
-import os
+import subprocess
+options=str(subprocess.check_output(["ls cv"])).split("\n")
+best=3.0
+checkpoint=""
+for option in options:
+	error=float(option[option.rfind("_"):option.rfind(".")])
+	if error < best:
+		error=best
+		checkpoint=option
 word = input("Make up a word and enter it here: ")
-response = str(os.system('''th sample.lua cv/lm_lstm_epoch21.60_1.3933.t7 -primetext "%s:" -temperature 0.5'''%(word)))
-print(response[:response.find("\n")])
+temperature = input("Set your desired temperature (0-1): ")
+response = str(subprocess.check_output(['''th sample.lua cv/%s -primetext "%s:" -temperature %s'''%(checkpoint,word,temperature)],shell=True))
+print(response)
