0c266dba126a06367dda250b862d69de135113aa
SCrawshaw18 <scott_crawshaw18@milton.edu>
2018-04-29 16:17:19 -0400

    edited mrclean so we can get more training data

--- mrclean.py
+++ mrclean.py
@@ -1,13 +1,5 @@
-
-# coding: utf-8
-
-# In[12]:
-
-
 import csv
-import json
 words = []
-
 bads=open("en.txt","r")
 listbads=bads.read().split("\n")
 with open('urban.csv') as csvfile:
@@ -27,15 +19,13 @@ with open('urban.csv') as csvfile:
             description=' '.join(description.split())
             description=description.replace(" ,",",")
             description=description.replace("\n","")
-            
             term=row[1].lower()
             mean=False
             for bad in listbads:
                 if bad.lower() in term.lower() or bad.lower() in description.lower():
                     mean=True
-            if len(description)>2 and term.find("/")==-1 and count % 25 == 0 and not mean:
+            if len(description)>2 and term.find("/")==-1 and count % 10 == 0 and not mean:
                 words.append("%s: %s \n"%(term,description))
-    print(words)
 file=open("input.txt","w")
 for word in words:
     file.write(word)
